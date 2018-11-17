fda6b34e0cabde3f2160fccb91dbb9db50e585be
Andreas Gropp <starmaerker@googlemail.com>
2018-11-17 08:01:17 +0100

    added get routes to index

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname+ '/public'));
-
-app.use((req, res, next) => {
+app.get('/home',(req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Sorry, this page does not exist.</h1></body></html>');
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/about',(req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/',(req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, hostname);
