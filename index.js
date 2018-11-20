10913b881ebba6c9d918cb2927751d7d747e7c92
Andreas Gropp <starmaerker@googlemail.com>
2018-11-20 05:53:50 +0100

    fixed problem with styles.css

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/css'));
 
 app.get('/home',(req, res, next) => {
     res.statusCode = 200;
@@ -23,5 +26,7 @@ app.get('/',(req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.sendFile(__dirname + '/public/index.html');
 });
+
+
 
 app.listen(port);
