1d67c4a8ba678bac200eb7a5b644606e7d0d829b
Andreas Gropp <starmaerker@googlemail.com>
2018-11-22 20:41:09 +0100

    using handlebars

--- index.js
+++ index.js
@@ -7,27 +7,21 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(morgan('dev'));
-app.engine('handlebar', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/css'));
 
 app.get('/home',(req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('home');
 });
 
 app.get('/about',(req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(__dirname + '/public/about.html');
+    res.render('about');
 });
 
 app.get('/',(req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('home');
 });
 
 app.listen(port);
