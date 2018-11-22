4335b4164e1e86b13b9bfcf997ae3aa4d46a0832
Andreas Gropp <starmaerker@googlemail.com>
2018-11-22 20:25:13 +0100

    setup handlebars

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.engine('handlebar', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.get('/home',(req, res, next) => {
     res.statusCode = 200;
