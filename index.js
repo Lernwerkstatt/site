705bbb7cc45dc35ab9d61e9bc27ec8b4e5297a45
Andreas Gropp <starmaerker@googlemail.com>
2018-12-11 18:07:02 +0100

    added console message and made remaining links clickable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const readJson = require("fs");
 
 const getCalendar = readJson.readFileSync("data/calendar.json", (err, data) => {
- if (err) throw err; 
+  if (err) throw err;
 });
 
 const convertToJson = JSON.parse(getCalendar);
@@ -18,7 +18,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/css"));
 
-app.get("/home", (req, res, next) => {  
+app.get("/home", (req, res, next) => {
   res.render("home", convertToJson);
 });
 
@@ -26,8 +26,24 @@ app.get("/about", (req, res, next) => {
   res.render("about");
 });
 
-app.get("/", (req, res, next) => {  
+app.get("/", (req, res, next) => {
   res.render("home", convertToJson);
+});
+
+app.get("/lernen", (req, res, next) => {
+  res.render("lernen");
+});
+
+app.get("/kurse", (req, res, next) => {
+  res.render("kurse");
+});
+
+app.get("/workshops", (req, res, next) => {
+  res.render("workshops");
+});
+
+app.get("/support", (req, res, next) => {
+  res.render("support");
 });
 
 const publicDir = require("path").join(__dirname, "/images");
