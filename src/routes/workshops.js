f16c707f4ebce23fde30a8b7b9d3eb173fec32f7
Daniel Streif <dan.streif@gmail.com>
2018-12-21 20:53:41 +0100

    Added path for workshops.json

--- src/routes/workshops.js
+++ src/routes/workshops.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const router = express.Router();
 
+const rootPath = path.join(__dirname, "../", "../");
+const workshopsPath = path.join(rootPath, "data/workshops.json");
+
 router.get("/workshops", (req, res) => {
-  res.render("workshops");
+  fs.readFile(workshopsPath, (err, data) => {
+    if (err) throw err;
+    res.render("workshops", JSON.parse(data));
+  });
 });
 
 module.exports = router;
