8ea0e2f4dcc2c990a4736b03986d6be56fedfbe5
Andreas Gropp <starmaerker@googlemail.com>
2018-12-21 17:37:54 +0100

    implemented functionality to get right icon from date

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const router = express.Router();
+const dayIcons = [
+  "https://img.icons8.com/office/30/000000/sunday.png",
+  "https://img.icons8.com/office/30/000000/monday.png",
+  "https://img.icons8.com/office/30/000000/tuesday.png",
+  "https://img.icons8.com/office/30/000000/wednesday.png",
+  "https://img.icons8.com/office/30/000000/thursday.png",
+  "https://img.icons8.com/office/30/000000/friday.png",
+  "https://img.icons8.com/office/30/000000/saturday.png"
+];
 
 const rootPath = path.join(__dirname, "../", "../");
 const calendarPath = path.join(rootPath, "data/calendar.json");
@@ -17,14 +26,24 @@ router.get("/", (req, res) => {
 router.get("/home", (req, res) => {
   fs.readFile(calendarPath, (err, data) => {
     if (err) throw err;
-    res.render("home", JSON.parse(data));
+    let input = JSON.parse(data);
+    res.render("home", createDayiconLink(input));
   });
 });
 
 function createDayiconLink(data) {
+  data.calendar.forEach(element => {
+    let tempDate = new Date(convertGermanToEnglishDate(element.date));
+    element["dayicon"] = dayIcons[tempDate.getDay()];
+  });
   return data;
 }
 
-module.exports = router;
+function convertGermanToEnglishDate(date) {
+  let result =
+    date.slice(3, 5) + "/" + date.slice(0, 2) + "/" + date.slice(6, 10);
 
-// "dayicon": "https://img.icons8.com/office/30/000000/monday.png",
+  return result;
+}
+
+module.exports = router;
