122b51cd893d34d230379e3156a24862b3ab0032
pheeria <olzhas.askar@gmail.com>
2018-12-22 14:50:39 +0100

    simplified code for dayicon addition

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const moment = require("moment");
 const router = express.Router();
-const dayIcons = [
-  "https://img.icons8.com/office/30/000000/sunday.png",
-  "https://img.icons8.com/office/30/000000/monday.png",
-  "https://img.icons8.com/office/30/000000/tuesday.png",
-  "https://img.icons8.com/office/30/000000/wednesday.png",
-  "https://img.icons8.com/office/30/000000/thursday.png",
-  "https://img.icons8.com/office/30/000000/friday.png",
-  "https://img.icons8.com/office/30/000000/saturday.png"
-];
 
 const rootPath = path.join(__dirname, "../", "../");
 const calendarPath = path.join(rootPath, "data/calendar.json");
@@ -19,7 +11,7 @@ router.get("/", (req, res) => {
   fs.readFile(calendarPath, (err, data) => {
     if (err) throw err;
     let input = JSON.parse(data);
-    res.render("home", createDayiconLink(input));
+    res.render("home", addDayiconLink(input));
   });
 });
 
@@ -27,23 +19,16 @@ router.get("/home", (req, res) => {
   fs.readFile(calendarPath, (err, data) => {
     if (err) throw err;
     let input = JSON.parse(data);
-    res.render("home", createDayiconLink(input));
+    res.render("home", addDayiconLink(input));
   });
 });
 
-function createDayiconLink(data) {
+function addDayiconLink(data) {
   data.calendar.forEach(element => {
-    let tempDate = new Date(convertGermanToEnglishDate(element.date));
-    element["dayicon"] = dayIcons[tempDate.getDay()];
+    let weekday = moment(element.date, "DD.MM.YYYY").format("dddd");
+    element["dayicon"] = `img/calendar/${weekday}.png`;
   });
   return data;
-}
-
-function convertGermanToEnglishDate(date) {
-  let result =
-    date.slice(3, 5) + "/" + date.slice(0, 2) + "/" + date.slice(6, 10);
-
-  return result;
 }
 
 module.exports = router;
