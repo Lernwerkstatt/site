92689bf0cb83702d8461cb3672708001ff88fc8a
Daniel Streif <dan.streif@gmail.com>
2018-12-25 16:11:47 +0100

    Align workshops.js with other js files

--- src/routes/workshops.js
+++ src/routes/workshops.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
+
 const router = express.Router();
 
-const workshopsPath = path.join(__dirname, "../../data/workshops.json");
-
 router.get("/workshops", (req, res) => {
-  fs.readFile(workshopsPath, (err, data) => {
+  fs.readFile("data/workshops.json", (err, data) => {
     if (err) throw err;
     res.render("workshops", JSON.parse(data));
   });
