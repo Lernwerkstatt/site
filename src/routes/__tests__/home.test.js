c5265f2edfe55b0f9f5011af98fd2bda49139248
Andreas Gropp <starmaerker@googlemail.com>
2019-01-03 22:25:27 +0100

    added another test

--- src/routes/__tests__/home.test.js
+++ src/routes/__tests__/home.test.js
@@ -25,8 +25,20 @@ describe("Test data", () => {
       }
     ]
   };
+  const dataWrong = {
+    calendar: [
+      {
+        name: "Webkurs",
+        date: "13.12.2018",
+        time: "18:00-20:00",
+        eventlink: "https://www.facebook.com/pg/dielernwerkstatt/events",
+        dayicon: "img/calendar/sunday.png"
+      }
+    ]
+  };
 
   test("check dayicon", () => {
     expect(home.prepareHome(stringifyData)).toEqual(dataExpected);
+    expect(home.prepareHome(stringifyData)).not.toEqual(dataWrong);
   });
 });
