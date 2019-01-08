4b34c038c07627602fba699e90071a1474f39722
Andreas Gropp <starmaerker@googlemail.com>
2019-01-08 17:30:37 +0100

    added basic blog functionality

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const router = express.Router();
 
+const blogPath = path.join(__dirname, "../../data/blog.json");
+
 router.get("/blog", (req, res) => {
-  res.render("blog");
+  fs.readFile(blogPath, (err, data) => {
+    if (err) throw err;
+    res.render("blog", JSON.parse(data));
+  });
 });
 
 module.exports = router;
