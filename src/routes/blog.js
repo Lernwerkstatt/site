dc476c19f0f1443eae3d2dcdc64c5f3521520771
Andreas Gropp <starmaerker@googlemail.com>
2019-01-14 10:20:22 +0100

    removed dead code and singlepost route

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -9,16 +9,13 @@ const blogPath = path.join(__dirname, "../../data/blog.json");
 router.get("/blog", (req, res) => {
   fs.readFile(blogPath, (err, data) => {
     if (err) throw err;
-    res.render("blog", JSON.parse(data));
-  });
-});
-
-router.get("/singlepost", (req, res) => {
-  fs.readFile(blogPath, (err, data) => {
-    if (err) throw err;
     const result = JSON.parse(data);
     const query = req.query.id;
-    res.render("singlepost", result.blogposts[query - 1]);
+    if (req.query.id) {
+      res.render("singlepost", result.blogposts[query - 1]);
+    } else {
+      res.render("blog", result);
+    }
   });
 });
 
