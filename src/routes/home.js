050a7dc18511b80f8ac8fb4f761cfe65c7672c95
Andreas Gropp <starmaerker@googlemail.com>
2019-03-12 09:45:53 +0100

    display latest blogpost in card

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,50 +4,32 @@ const fs = require("fs");
 const moment = require("moment");
 const posts = require("../services/database");
 
-let allPosts;
-let dateArray = [];
-
-async function test() {
-  allPosts = await posts.allPosts.then(datas => (allPosts = datas.blogs));
-  for (let i = 0; i < allPosts.length; i++) {
-    dateArray.push(allPosts[i].date);
-  }
-  console.log(dateArray);
-
-  dateArray.sort(date_sort);
-
-  console.log(dateArray);
-}
-
-test();
-
-let date_sort = function(date1, date2) {
-  if (date1 > date2) return 1;
-  if (date1 < date2) return -1;
-  return 0;
-};
-
 const router = express.Router();
 
 const homePath = path.join(__dirname, "../../data/home.json");
 
-function prepareHome(data) {
-  const result = JSON.parse(data);
+async function prepareHome(res, data) {
+  try {
+    const result = JSON.parse(data);
 
-  result.calendar.forEach(element => {
-    const weekday = moment(element.date, "DD.MM.YYYY")
-      .format("dddd")
-      .toLowerCase();
-    element.dayicon = `img/calendar/${weekday}.png`;
-  });
-
-  return result;
+    result.calendar.forEach(element => {
+      const weekday = moment(element.date, "DD.MM.YYYY")
+        .format("dddd")
+        .toLowerCase();
+      element.dayicon = `img/calendar/${weekday}.png`;
+    });
+    const latestBlogPost = await posts.allPosts;
+    result.card[2].text = latestBlogPost.blogs[0].summary;
+    res.render("home", result);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 router.get("/", (req, res) => {
   fs.readFile(homePath, (err, data) => {
     if (err) throw err;
-    res.render("home", prepareHome(data));
+    prepareHome(res, data);
   });
 });
 
