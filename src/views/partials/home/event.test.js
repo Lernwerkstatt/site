491c2df77bf1e3937f1463006ffb4aa6fff6021e
Andreas Gropp <starmaerker@googlemail.com>
2019-03-22 11:11:17 +0100

    update event.test.js

--- src/views/partials/home/event.test.js
+++ src/views/partials/home/event.test.js
@@ -1,9 +1,11 @@
 const event = require("./event.hbs");
+const modal = require("./modal.hbs");
 
 describe("event.hbs", () => {
   it("should match the snapshot", () => {
     expect(
       event({
+        id: "seven",
         name: "Webkurs",
         date: "13.12.2018",
         time: "18:00-20:00",
