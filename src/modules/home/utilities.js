d445ebedb894a59108ba730f52e32f6f1c9da616
pheeria <olzhas.askar@gmail.com>
2019-03-28 19:31:49 +0100

    Make fb work

--- src/modules/home/utilities.js
+++ src/modules/home/utilities.js
@@ -1,16 +1,14 @@
 const moment = require("moment");
 
 function prepareHome(data) {
-  const result = JSON.parse(data);
-
-  result.calendar.forEach(element => {
+  data.forEach(element => {
     const weekday = moment(element.date, "DD.MM.YYYY")
       .format("dddd")
       .toLowerCase();
     element.dayicon = `img/calendar/${weekday}.png`;
   });
 
-  return result;
+  return data;
 }
 
 module.exports = { prepareHome };
