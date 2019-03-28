97220fb4987175a2af018ceb71663278e73da684
pheeria <olzhas.askar@gmail.com>
2019-03-28 19:58:07 +0100

    Extract utils

--- src/services/events.js
+++ src/services/events.js
@@ -1,49 +1,15 @@
 /* eslint camelcase: "off" */
 
 const fb = require("fb");
-const moment = require("moment");
 const { facebookToken, facebookPageId } = require("../../config/secrets");
+const {
+  extractNearestDate,
+  stringifyEventDate,
+  createEventLink
+} = require("./fbUtils");
 
 const FB = new fb.Facebook({ version: "v3.2" });
 FB.setAccessToken(facebookToken);
-
-const extractNearestDate = event => {
-  const result = {
-    start_time: event.start_time,
-    end_time: event.end_time
-  };
-
-  if (event.event_times) {
-    const upcoming = event.event_times.filter(
-      t => new Date(t.start_time) > new Date()
-    );
-    const sorted = upcoming.sort(
-      (a, b) => new Date(a.start_time) > new Date(b.start_time)
-    );
-
-    result.start_time = sorted[0].start_time;
-    result.end_time = sorted[0].end_time;
-  }
-
-  return result;
-};
-
-const stringifyEventDate = eventDate => {
-  let result = "";
-  const start = moment(eventDate.start_time);
-  const end = moment(eventDate.end_time);
-
-  if (start.diff(end, "days") === 0) {
-    result = `${start.format("DD.MM.YYYY HH:mm")} - ${end.format("HH:mm")}`;
-  } else {
-    result = `${start.format("DD.MM.YYYY HH:mm")} - ${end.format(
-      "DD.MM.YYYY HH:mm"
-    )}`;
-  }
-  return result;
-};
-
-const createEventLink = id => `https://www.facebook.com/events/${id}`;
 
 const getEvents = () =>
   new Promise((resolve, reject) => {
@@ -80,8 +46,5 @@ const getEvents = () =>
   });
 
 module.exports = {
-  extractNearestDate,
-  stringifyEventDate,
-  createEventLink,
   getEvents
 };
