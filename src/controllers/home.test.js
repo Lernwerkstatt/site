3d965aa32f278af19cc64912a911a5e1645a6fed
pheeria <olzhas.askar@gmail.com>
2019-05-22 12:08:15 +0200

    Try to fix tests

--- src/controllers/home.test.js
+++ src/controllers/home.test.js
@@ -1,5 +1,9 @@
 const { getIndex } = require("./home");
 
+jest.mock("../routes/posts");
+jest.mock("../services/events");
+jest.mock("../utilities/facebook");
+
 describe("Home controller", () => {
   it("should provide proper view name", async () => {
     const request = {};
