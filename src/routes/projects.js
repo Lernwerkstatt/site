9b416853e9341524e03b896173ed6e56d98d317e
pheeria <olzhas.askar@gmail.com>
2019-06-12 23:31:10 +0200

    Workshops >> Projects

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 
 const router = express.Router();
 
-const projectsPath = path.join(__dirname, "../../data/workshops.json");
+const projectsPath = path.join(__dirname, "../../data/projects.json");
 
 router.get("/projects", (req, res) => {
   fs.readFile(projectsPath, (err, data) => {
     if (err) throw err;
-    res.render("workshops", JSON.parse(data));
+    res.render("projects", JSON.parse(data));
   });
 });
 
