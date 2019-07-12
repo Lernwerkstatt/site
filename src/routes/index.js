8f921be6e196d9830f016b04f98ac568a0f0d585
cappyccino <cchausfeld@gmail.com>
2019-07-12 11:53:25 +0200

    WIP Created partners page
    TODO:
    Get JSON file working to display list of partners
    Resize/reformat images to be consistent

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,17 +7,19 @@ const aboutRouter = require("./about.js");
 const blogsRouter = require("./blogs.js");
 const contactRouter = require("./contact.js");
 const learnRouter = require("./learn.js");
-const supportRouter = require("./support.js");
+const partners = require("./partners.js");
 const projects = require("./projects.js");
 const subscribeRouter = require("./subscribe.js");
+const supportRouter = require("./support.js");
 
 router.use(homeRouter.router);
 router.use(aboutRouter);
 router.use(blogsRouter);
 router.use(contactRouter);
 router.use(learnRouter);
-router.use(supportRouter);
+router.use(partners);
 router.use(projects);
 router.use(subscribeRouter);
+router.use(supportRouter);
 
 module.exports = router;
