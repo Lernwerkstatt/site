eb7ca70da5dd9ceb79977ef967389d949c759c1c
Daniel Streif <dan.streif@gmail.com>
2019-08-20 14:08:34 +0200

    Improve readability

--- browserSync.sh
+++ browserSync.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sleep .5
-browser-sync start --no-online --no-ui --port 3001 --proxy "localhost:3000" --files "src/views/" "static/"
+browser-sync start \
+    --no-online \
+    --no-ui \
+    --port 3001 \
+    --proxy "localhost:3000" \
+    --files "src/views/" "static/"
