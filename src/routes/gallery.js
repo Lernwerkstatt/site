d7b2c8136562fe7f9305eaa503b412ec8ebde60c
Daniel Streif <dan.streif@gmail.com>
2019-09-20 15:20:06 +0200

    Add header link

--- src/routes/gallery.js
+++ src/routes/gallery.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/gallery", (req, res) => {
-  result = {
+  const result = {
     apiKey: process.env.PIXLEE_INSTAGRAM_API_KEY
   };
   res.render("gallery", result);
