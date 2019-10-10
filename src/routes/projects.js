b8d4cac6d4e69b3223a6d4e39023e033a8507fb2
Zharzhan Kulmyrza <zharzhan@gmail.com>
2019-10-10 19:13:12 +0200

    i18 projects

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const projects = require("../../data/projects.json");
 
 const router = express.Router();
 
 router.get("/projects", (req, res) => {
-  res.render("projects", projects);
+  res.render("projects");
 });
 
 module.exports = router;
