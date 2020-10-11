3026fb267a41c22c1f6c40ef054c8e1b35d7042a
pheeria <olzhas.askar@deliveryhero.com>
2020-10-11 18:13:12 +0200

    Add tests for translations

--- cypress/integration/home/header.spec.js
+++ cypress/integration/home/header.spec.js
@@ -9,35 +9,39 @@ describe("Header links", () => {
     {
       link: "Unsere Werte",
       href: "values",
-      title: "Unsere Werte"
+      title: "Unsere Werte",
     },
     {
       link: "Projekte",
       href: "projects",
-      title: "Projekte"
+      title: "Projekte",
     },
     {
       link: "Blog",
       href: "blogs",
-      title: "Blog"
+      title: "Blog",
+    },
+    {
+      link: "Podcast",
+      href: "podcasts",
+      title: "Podcast",
     },
     {
       link: "Menschen",
       href: "team",
-      title: "Wir sind Die Lernwerkstatt"
+      title: "Wir sind Die Lernwerkstatt",
     },
-    {
-      link: "Galerie",
-      href: "gallery",
-      title: "Galerie"
-    }
+    // Doesn't work yet for cookie reasons
+    // {
+    //   link: "Galerie",
+    //   href: "gallery",
+    //   title: "Galerie"
+    // }
   ];
 
-  pages.forEach(page => {
+  pages.forEach((page) => {
     it(`should go to ${page.link}`, () => {
-      cy.get("header  .nav-link")
-        .contains(page.link)
-        .click();
+      cy.get("header .nav-link").contains(page.link).click();
 
       cy.location("pathname").should("include", page.href);
 
