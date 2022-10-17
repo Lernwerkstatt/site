3a3430f9c0e495b8878adb8287692c1bc55612a8
pheeria <olzhas.askar@deliveryhero.com>
2022-10-17 18:02:39 +0200

    fix: jest's breaking changes

--- config/handlebarsLoader.js
+++ config/handlebarsLoader.js
@@ -1,9 +1,10 @@
 module.exports = {
   process(src) {
-    return `
-    const Handlebars = require('handlebars');
-    Handlebars.registerHelper('__', function(){ return 'i18n' });
-    module.exports = Handlebars.compile(\`${src}\`);
-    `;
-  }
+    return {
+      code: `
+        const Handlebars = require('handlebars');
+        Handlebars.registerHelper('__', function(){ return 'i18n' });
+        module.exports = Handlebars.compile(\`${src}\`);`,
+    };
+  },
 };
