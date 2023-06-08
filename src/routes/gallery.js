7734946047962a20dcfb4ef7e506dd5d6fc0c49e
Dan <dan.streif@gmail.com>
2023-06-08 21:39:41 +0200

    feat(gallery): switch from Pixlee to LightWidget

--- src/routes/gallery.js
+++ src/routes/gallery.js
@@ -3,11 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/gallery", (req, res) => {
-  const result = {
-    apiKey: process.env.PIXLEE_INSTAGRAM_API_KEY,
-    widgetID: process.env.PIXLEE_INSTAGRAM_WIDGET_ID
-  };
-  res.render("gallery", result);
+  res.render("gallery");
 });
 
 module.exports = router;
